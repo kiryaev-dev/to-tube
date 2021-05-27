d9bedbcba6d334f1d405a9b3e97d9678f2ba671d
kiryaev-dev <kiryaev.dev@gmail.com>
2021-05-27 15:58:35 +0500

    Created index

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as ReactDOM from 'react-dom'
+import * as React from 'react'
+
+import './index.css'
+
+const root = document.getElementById("root") as HTMLElement
+
+ReactDOM.render(
+    <></>,
+    root
+)
